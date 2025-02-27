0b637a26cb891539e37cb28b72c4b0f4fc086aab
mdmhrz <razufreelence@gmail.com>
2025-02-27 23:05:18 +0600

    Brand Color JS Added

---
+++ script.js
@@ -0,0 +1,9 @@
+const ringButtons = document.querySelectorAll('.ring-button');
+
+for (let i = 0; i < ringButtons.length; i++) {
+    let ringButton = ringButtons[i]
+    ringButton.addEventListener('click', function () {
+
+    })
+}
+
